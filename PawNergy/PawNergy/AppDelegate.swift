23a541c58f9106f52ab7d7c50a80c560a59414fe
Triet Le <letrongtriet@gmail.com>
2019-02-20 08:13:17 +0200

    Initial commit

---
+++ PawNergy/PawNergy/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  PawNergy
+//
+//  Created by Trong Triet Le on 20/02/2019.
+//  Copyright © 2019 Triet Le. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
